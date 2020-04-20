77258c599ca9808fa7a9a170eb984afd14ddfc8a
Kevin Spring <kevinspring93@hotmail.com>
2020-04-20 11:47:45 +0200

    store data in localstorage

--- src/components/Form.js
+++ src/components/Form.js
@@ -13,25 +13,46 @@ class Form extends Component{
         }
     }
 
+    handleOnChangeName = (e) => {
+
+        this.setState({name: e.target.value})
+    }
+    handleOnChangeTime = (e) => {
+
+        this.setState({bookedTime: e.target.value})
+    }
+    handleOnChangeMobile = (e) => {
+
+        this.setState({phoneNr: e.target.value})
+    }
+
+
+    handleOnSubmit(e){
+        e.preventDefault();
+        //this.setState({})
+    }
 
     render() {
         return (
             <div>
-                <form>
-                    <input type={"text"} placeholder={"Name"}></input>
-                    <input type={"text"} placeholder={"Pick a day!"}></input>
-                    <input type={"number"} placeholder={"Telephone"}></input>
-                    <input type={"submit"} value={"Book studio"} ></input>
+                <form onSubmit={this.handleOnSubmit}>
+                    <input type={"text"} placeholder={"Name"} onChange = {this.handleOnChangeName}></input>
+                    <input type={"text"} placeholder={"Pick a day!"} onChange = {this.handleOnChangeTime}></input>
+                    <input type={"number"} placeholder={"Telephone"} onChange = {this.handleOnChangeMobile}></input>
+                    <button type={"submit"} onSubmit={this.handleOnSubmit}>Book studio</button>
                 </form>
+
+                <div>{this.state.name}</div>
+                <div>{this.state.bookedTime}</div>
+                <div>{this.state.phoneNr}</div>
             </div>
         )
     }
 
 
-
-
-
-
 }
 
 export default Form;
+
+//Spara state i localStorage
+//this and arrow function
