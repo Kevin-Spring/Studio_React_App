984ed4e4be280fc2ca90c5f0bebb232d8e43bbd3
Kevin Spring <kevinspring93@hotmail.com>
2020-04-20 13:25:36 +0200

    localstorage implemented

--- src/components/Form.js
+++ src/components/Form.js
@@ -16,20 +16,25 @@ class Form extends Component{
     handleOnChangeName = (e) => {
 
         this.setState({name: e.target.value})
+        localStorage.setItem("Name", e.target.value);
+        
     }
     handleOnChangeTime = (e) => {
 
         this.setState({bookedTime: e.target.value})
+        localStorage.setItem("BookedTime", e.target.value);
+        
     }
     handleOnChangeMobile = (e) => {
 
         this.setState({phoneNr: e.target.value})
+        localStorage.setItem("PhoneNr", e.target.value);
     }
 
 
     handleOnSubmit(e){
         e.preventDefault();
-        //this.setState({})
+        
     }
 
     render() {
@@ -39,7 +44,7 @@ class Form extends Component{
                     <input type={"text"} placeholder={"Name"} onChange = {this.handleOnChangeName}></input>
                     <input type={"text"} placeholder={"Pick a day!"} onChange = {this.handleOnChangeTime}></input>
                     <input type={"number"} placeholder={"Telephone"} onChange = {this.handleOnChangeMobile}></input>
-                    <button type={"submit"} onSubmit={this.handleOnSubmit}>Book studio</button>
+                    <button type={"submit"} >Book studio</button>
                 </form>
 
                 <div>{this.state.name}</div>
@@ -49,10 +54,6 @@ class Form extends Component{
         )
     }
 
-
 }
 
 export default Form;
-
-//Spara state i localStorage
-//this and arrow function
