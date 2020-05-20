ce19301120b6b2caf688ade8fe335b2d0242c7a6
Kevin Spring <kevinspring93@hotmail.com>
2020-05-20 15:28:21 +0200

    make the profilepicture be shown in top right corner when saved

--- src/components/AuthUser/UserProfile.js
+++ src/components/AuthUser/UserProfile.js
@@ -11,16 +11,15 @@ class UserProfile extends Component {
 
     state = {
 
-        image: " ",
+        image: undefined,
         user: null
 
     }
 
-    componentDidMount() {
-        firebase.auth().onAuthStateChanged((user) => {
+    //To show uploaded file
+    componentDidMount(e) {
             this.setState({ user: this.props.userData });
-            console.log(user);
-        })
+           
     }
 
 
@@ -45,7 +44,59 @@ class UserProfile extends Component {
     }
 
 
+    //To upload file
+    async profilePic(e) {
 
+         //Get elements
+         let uploader = document.getElementById("uploader");
+         let user = firebase.auth().currentUser.uid;
+        
+             // get File
+             let file = e.target.files[0];
+             console.log(file)
+       
+             // create storge ref
+             let storageRef = firebase.storage().ref("profilepictures/" + "users/" + user + "/" + file.name);
+               //uplad file
+               let task = storageRef.put(file);
+               //progress bar
+               task.on('state_changed',
+               
+                 function progress(snapshot){
+                     let percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                     uploader.value = percentage;
+                 },
+         
+                 function error(err){
+                    console.log(err);
+                 },
+         
+                 function complete(){
+                    console.log("successfully uploaded");
+                 }
+         
+               )
+
+               const img = document.querySelector("#img");
+
+          firebase.auth().onAuthStateChanged(user =>{
+             if(user){
+                 firebase.storage().ref("profilepictures/" + "users/" + user.uid + "/" + file.name).getDownloadURL().then(imgUrl => {
+                     img.src = imgUrl;
+                 })
+             }
+         })
+    }
+
+
+    //To upload file
+    onSubmitToFirebase(e) {
+         e.preventDefault();
+
+         //let file = e.target.files[0];
+         
+        
+    }
 
     render() {
 
@@ -64,9 +115,25 @@ class UserProfile extends Component {
                             <section className={"contact"}>
                                 <h2 className={"contact__header"}>Welcome {this.props.userData} </h2>
 
-                               
+                                <form onSubmit={this.onSubmitToFirebase.bind(this)}>
+                                    <div className={"form__container"}>
+                                        <div className={"form__group field"}>
+                                            <input type={"file"} className={"form__field img"} onChange={this.profilePic.bind(this)} name={"file"} id={"fileButton"} />
+                                        </div>
+                                        <progress value="0" max="100" id="uploader">0%</progress>
+                                        <br />
 
+                                        <img src="placeholder.jpg" alt="" id={"img"}></img>
 
+                                        <div className={"btn-animation"}>
+                                            <button className={"form-btn form__btn-underline"}>Spara</button>
+                                        </div>
+                                    </div>
+                                    
+                                </form>
+
+                                
+                                
 
                                 <button onClick={this.logOut.bind(this)} className={"form-btn"}>Logout</button>
                                 <button onClick={this.deleteAcc.bind(this)} className={"form-btn"}>DELTE ACC</button>
@@ -79,7 +146,8 @@ class UserProfile extends Component {
 
 
                     (
-                        <div></div>
+                        <div>
+                                </div>
                     )
 
                 }
--- src/components/FirebaseConfig.js
+++ src/components/FirebaseConfig.js
@@ -1,6 +1,7 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
+import "firebase/storage";
 require('dotenv').config();
 
 const firebaseConfig = {
