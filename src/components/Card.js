6a9455c7ede39f2fedc7c6fb4027f48ac0db426b
Kevin Spring <kevinspring93@hotmail.com>
2020-06-08 11:47:57 +0200

    react modal

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,9 +2,22 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import firebase from "./FirebaseConfig";
 import "../style/_card.scss";
+import BookingFormModal from "./bookingFormModal";
 
 
 class Card extends Component {
+
+    state = {
+        openModal: false,
+        confirmation: undefined
+    }
+
+    openModalEvent(){
+        this.setState({openModal: !this.state.openModal})
+    }
+    closeModalEvent(){
+        this.setState({openModal: false})
+    }
 
     renderLoginReq(){
 
@@ -50,26 +63,33 @@ class Card extends Component {
             alert("Please create an account before booking a studio")
         }
 
-        /* IF CONDITION ANVÄNDARE MÅSTE VARA INLOGGAD */
-
     }
 
     render() {
         return (
-            <div>
+            <>
+            <BookingFormModal openModal={this.state.openModal} closeModal={this.closeModalEvent.bind(this)} 
+                callback={ (time)=> {
+                this.setState({confirmation: time})
+            }}/>
 
+            
                 <div className={"cards__wrapper"}>
+                
 
-                    <div className={"card"}>
-                        <img className={"card-img-top"} src={this.props.image} alt="studio 1" />
-                        <div className={"card__body"}>
-                            <h5 className={"card__title"}>{this.props.title}</h5>
-                            <p className={"card__text"}>{this.props.description} <span className={"card__price"}>{this.props.price} kr</span></p>
-                            <Link to={"/handle-bookings"} className={"card__button"} onClick = {this.onClickBookStudio.bind(this)}>Book</Link>
-                        </div>
+                <div className={"card"}>
+                    <img className={"card-img-top"} src={this.props.image} alt="studio 1" />
+                    <div className={"card__body"}>
+                        <h5 className={"card__title"}>{this.props.title}</h5>
+                        <p className={"card__text"}>{this.props.description} <span className={"card__price"}>{this.props.price} kr</span></p>
+                        <button className={"card__cancel"} onClick = {this.openModalEvent.bind(this)}>MODAL EVENT</button>
+                        <Link to={"/handle-bookings"} className={"card__button"} onClick = {this.onClickBookStudio.bind(this)}>Book</Link>
+                        {this.state.confirmation ?  <div> <br/> Your booked time: {this.state.confirmation} <br/> </div> : <span></span> } 
                     </div>
                 </div>
             </div>
+             
+            </>
         )
     }
 
---
+++ src/components/bookingFormModal.js
@@ -0,0 +1,109 @@
+import React, { Component } from 'react';
+//import firebase from "./FirebaseConfig";
+import Modal from "react-modal";
+import "../style/_Form.scss";
+
+
+const customStyles = {
+    content : {
+      top                   : '60%',
+      left                  : '50%',
+      right                 : 'auto',
+      bottom                : 'auto',
+      marginRight           : '-50%',
+      transform             : 'translate(-50%, -50%)'
+    }
+  };
+
+ 
+
+export default class BookingFormModal extends Component {
+
+
+    handleConfirmation(e){
+        e.preventDefault();
+        this.props.callback(e.target.elements.time.value)
+    }
+
+    //onSubmit={this.handleOnSubmit.bind(this)}
+    //även tagit bort method="post" encType="text/plain" från form
+
+
+   /*  handleOnSubmit(e) {
+        e.preventDefault();
+        
+        const userId = firebase.auth().currentUser.uid;
+        
+        const db = firebase.firestore();
+    
+        if (firebase.auth().currentUser) {
+    
+                db.collection("bookingFormData").doc(userId)
+                .collection("personalData").add({
+                    name: e.target.elements.name.value,
+                    telephone: e.target.elements.telephone.value,
+                    time: e.target.elements.time.value,
+                    message: e.target.elements.textarea.value
+                });
+    
+        } else {
+            alert("Please create an account before booking a studio")
+        }
+    
+        this.setState({ condition: true })
+    
+    } */
+
+
+    render() {
+        return (
+            <>
+
+            <Modal
+                isOpen={this.props.openModal}
+                onRequestClose={this.props.closeModal}
+                style={customStyles}
+                contentLabel="Booking Form"
+            >
+
+            <section className={"contact"}>
+                       
+                        <div className={"btn-animation"}>
+                                <button onClick={this.props.closeModal} className={"form-btn"}>X</button>
+                            </div>
+
+                        <form onSubmit={this.handleConfirmation.bind(this)}>
+                            <div className={"form__container"}>
+                                <div className={"form__group field"}>
+                                    <input type={"text"} className={"form__field"} placeholder={"Name"} name={"name"} id={'name'} required />
+                                    <label htmlFor={"name"} className={"form__label"}>Name</label>
+                                </div>
+                                <div className={"form__group field"}>
+                                    <input type={"text"} className={"form__field"} name={"time"} id={"input__time"} placeholder={"Time"}
+                                        required />
+                                    <label htmlFor={"input__time"} className={"form__label"}>What Time?</label>
+                                </div>
+                                <div className={"form__group field"}>
+                                    <input type="number" className={"form__field"} name={"telephone"} id={"input__telephone"} placeholder={"Telephone"}
+                                         required />
+                                    <label htmlFor={"input__telephone"} className={"form__label"}>Telephone</label>
+                                </div>
+                                <div className={"form__group field"}>
+                                    <textarea name={"textarea"} className={"form__field"} id={"textarea"} cols={"30"} rows={"10"}
+                                        placeholder={"Other thoughts?"}></textarea>
+                                    <label htmlFor={"textarea"} className={"form__label"}>Anything else?</label>
+                                </div>
+                                <div className={"btn-animation"}>
+                                    <button type={"submit"} className={"form-btn"}>Confirm!</button>
+                                </div>
+                            </div>
+                        </form>
+                    </section>
+
+                </Modal>
+                
+            </>
+        )
+    }
+}
+
